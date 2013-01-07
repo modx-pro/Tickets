73c17683897344060239a1574ebb203a5dc3f79c
Bezumkin <bezumkin@yandex.ru>
2013-01-07 11:25:47 +0700

    To prevent update, chunks are no longer static.

--- _build/data/transport.chunks.php
+++ _build/data/transport.chunks.php
@@ -13,9 +13,9 @@ $chunks[0]->fromArray(array(
 	,'name' => 'tpl.Tickets.form.create'
 	,'description' => 'Chunk for creation of new ticket'
 	,'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/form_create.chunk.tpl')
-	,'source' => 1
-	,'static' => 1
-	,'static_file' => 'core/components/tickets/elements/chunks/form_create.chunk.tpl'
+	//,'source' => 1
+	//,'static' => 1
+	//,'static_file' => 'core/components/tickets/elements/chunks/form_create.chunk.tpl'
 ),'',true,true);
 
 $chunks[1]= $modx->newObject('modChunk');
@@ -24,9 +24,9 @@ $chunks[1]->fromArray(array(
 	,'name' => 'tpl.Tickets.form.update'
 	,'description' => 'Chunk for update existing ticket'
 	,'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/form_update.chunk.tpl')
-	,'source' => 1
-	,'static' => 1
-	,'static_file' => 'core/components/tickets/elements/chunks/form_update.chunk.tpl'
+	//,'source' => 1
+	//,'static' => 1
+	//,'static_file' => 'core/components/tickets/elements/chunks/form_update.chunk.tpl'
 ),'',true,true);
 
 $chunks[2]= $modx->newObject('modChunk');
@@ -35,9 +35,9 @@ $chunks[2]->fromArray(array(
 	,'name' => 'tpl.Tickets.form.section.row'
 	,'description' => 'Chunk for template one section of form select'
 	,'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/form_section_row.chunk.tpl')
-	,'source' => 1
-	,'static' => 1
-	,'static_file' => 'core/components/tickets/elements/chunks/form_section_row.chunk.tpl'
+	//,'source' => 1
+	//,'static' => 1
+	//,'static_file' => 'core/components/tickets/elements/chunks/form_section_row.chunk.tpl'
 
 ),'',true,true);
 
@@ -47,9 +47,9 @@ $chunks[3]->fromArray(array(
 	,'name' => 'tpl.Tickets.form.preview'
 	,'description' => 'Chunk for preview ticket before publish'
 	,'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/form_preview.chunk.tpl')
-	,'source' => 1
-	,'static' => 1
-	,'static_file' => 'core/components/tickets/elements/chunks/form_preview.chunk.tpl'
+	//,'source' => 1
+	//,'static' => 1
+	//,'static_file' => 'core/components/tickets/elements/chunks/form_preview.chunk.tpl'
 
 ),'',true,true);
 
@@ -59,9 +59,9 @@ $chunks[4]->fromArray(array(
 	,'name' => 'tpl.Tickets.comment.form'
 	,'description' => 'The add comment form. Can either be a chunk name or value. If set to a value, will override the chunk.'
 	,'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/comment_form.chunk.tpl')
-	,'source' => 1
-	,'static' => 1
-	,'static_file' => 'core/components/tickets/elements/chunks/comment_form.chunk.tpl'
+	//,'source' => 1
+	//,'static' => 1
+	//,'static_file' => 'core/components/tickets/elements/chunks/comment_form.chunk.tpl'
 ),'',true,true);
 
 $chunks[5]= $modx->newObject('modChunk');
@@ -70,9 +70,9 @@ $chunks[5]->fromArray(array(
 	,'name' => 'tpl.Tickets.comment.one.auth'
 	,'description' => 'A Chunk for the comment itself. For authorized users.'
 	,'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/comment_one_auth.chunk.tpl')
-	,'source' => 1
-	,'static' => 1
-	,'static_file' => 'core/components/tickets/elements/chunks/comment_one_auth.chunk.tpl'
+	//,'source' => 1
+	//,'static' => 1
+	//,'static_file' => 'core/components/tickets/elements/chunks/comment_one_auth.chunk.tpl'
 ),'',true,true);
 
 $chunks[6]= $modx->newObject('modChunk');
@@ -81,9 +81,9 @@ $chunks[6]->fromArray(array(
 	,'name' => 'tpl.Tickets.comment.one.guest'
 	,'description' => 'A Chunk for the comment itself. For guests.'
 	,'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/comment_one_guest.chunk.tpl')
-	,'source' => 1
-	,'static' => 1
-	,'static_file' => 'core/components/tickets/elements/chunks/comment_one_guest.chunk.tpl'
+	//,'source' => 1
+	//,'static' => 1
+	//,'static_file' => 'core/components/tickets/elements/chunks/comment_one_guest.chunk.tpl'
 ),'',true,true);
 
 $chunks[7]= $modx->newObject('modChunk');
@@ -92,9 +92,9 @@ $chunks[7]->fromArray(array(
 	,'name' => 'tpl.Tickets.comment.wrapper'
 	,'description' => 'A Chunk for the outer wrapper for comments.'
 	,'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/comment_wrapper.chunk.tpl')
-	,'source' => 1
-	,'static' => 1
-	,'static_file' => 'core/components/tickets/elements/chunks/comment_wrapper.chunk.tpl'
+	//,'source' => 1
+	//,'static' => 1
+	//,'static_file' => 'core/components/tickets/elements/chunks/comment_wrapper.chunk.tpl'
 ),'',true,true);
 
 $chunks[8]= $modx->newObject('modChunk');
@@ -103,9 +103,9 @@ $chunks[8]->fromArray(array(
 	,'name' => 'tpl.Tickets.comment.login'
 	,'description' => 'The portion to show when the user is not logged in. Can either be a chunk name or value. If set to a value, will override the chunk.'
 	,'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/comment_login.chunk.tpl')
-	,'source' => 1
-	,'static' => 1
-	,'static_file' => 'core/components/tickets/elements/chunks/comment_login.chunk.tpl'
+	//,'source' => 1
+	//,'static' => 1
+	//,'static_file' => 'core/components/tickets/elements/chunks/comment_login.chunk.tpl'
 ),'',true,true);
 
 $chunks[9]= $modx->newObject('modChunk');
@@ -114,9 +114,9 @@ $chunks[9]->fromArray(array(
 	,'name' => 'tpl.Tickets.ticket.latest'
 	,'description' => 'The chunk for templating latest tickets'
 	,'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/ticket_latest.chunk.tpl')
-	,'source' => 1
-	,'static' => 1
-	,'static_file' => 'core/components/tickets/elements/chunks/ticket_latest.chunk.tpl'
+	//,'source' => 1
+	//,'static' => 1
+	//,'static_file' => 'core/components/tickets/elements/chunks/ticket_latest.chunk.tpl'
 ),'',true,true);
 
 $chunks[10]= $modx->newObject('modChunk');
@@ -125,9 +125,9 @@ $chunks[10]->fromArray(array(
 	,'name' => 'tpl.Tickets.comment.latest'
 	,'description' => 'The chunk for templating latest comments'
 	,'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/comment_latest.chunk.tpl')
-	,'source' => 1
-	,'static' => 1
-	,'static_file' => 'core/components/tickets/elements/chunks/comment_latest.chunk.tpl'
+	//,'source' => 1
+	//,'static' => 1
+	//,'static_file' => 'core/components/tickets/elements/chunks/comment_latest.chunk.tpl'
 ),'',true,true);
 
 $chunks[11]= $modx->newObject('modChunk');
@@ -136,9 +136,9 @@ $chunks[11]->fromArray(array(
 	,'name' => 'tpl.Tickets.comment.email.owner'
 	,'description' => 'The chunk for notification of new comments of ticket owner'
 	,'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/comment_email_owner.chunk.tpl')
-	,'source' => 1
-	,'static' => 1
-	,'static_file' => 'core/components/tickets/elements/chunks/comment_email_owner.chunk.tpl'
+	//,'source' => 1
+	//,'static' => 1
+	//,'static_file' => 'core/components/tickets/elements/chunks/comment_email_owner.chunk.tpl'
 ),'',true,true);
 
 $chunks[12]= $modx->newObject('modChunk');
@@ -147,9 +147,9 @@ $chunks[12]->fromArray(array(
 	,'name' => 'tpl.Tickets.comment.email.reply'
 	,'description' => 'The chunk for notification of user who replied to a comment'
 	,'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/comment_email_reply.chunk.tpl')
-	,'source' => 1
-	,'static' => 1
-	,'static_file' => 'core/components/tickets/elements/chunks/comment_email_reply.chunk.tpl'
+	//,'source' => 1
+	//,'static' => 1
+	//,'static_file' => 'core/components/tickets/elements/chunks/comment_email_reply.chunk.tpl'
 ),'',true,true);
 
 $chunks[13]= $modx->newObject('modChunk');
@@ -158,9 +158,9 @@ $chunks[13]->fromArray(array(
 	,'name' => 'tpl.Tickets.list.row'
 	,'description' => 'The chunk for list tickets in getResources'
 	,'snippet' => file_get_contents($sources['source_core'].'/elements/chunks/ticket_list_row.chunk.tpl')
-	,'source' => 1
-	,'static' => 1
-	,'static_file' => 'core/components/tickets/elements/chunks/ticket_list_row.chunk.tpl'
+	//,'source' => 1
+	//,'static' => 1
+	//,'static_file' => 'core/components/tickets/elements/chunks/ticket_list_row.chunk.tpl'
 ),'',true,true);
 
 
