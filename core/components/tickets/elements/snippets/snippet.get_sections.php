757364e5dde53b5752c5a841c3856de40bf3e1db
Bezumkin <bezumkin@yandex.ru>
2014-04-06 12:04:22 +0700

    Improved loading of pdoTools in snippets

--- core/components/tickets/elements/snippets/snippet.get_sections.php
+++ core/components/tickets/elements/snippets/snippet.get_sections.php
@@ -5,8 +5,16 @@ $Tickets = $modx->getService('tickets','Tickets',$modx->getOption('tickets.core_
 $Tickets->initialize($modx->context->key);
 /** @var pdoFetch $pdoFetch */
 $fqn = $modx->getOption('pdoFetch.class', null, 'pdotools.pdofetch', true);
-if (!$pdoClass = $modx->loadClass($fqn, '', false, true)) {return false;}
-$pdoFetch = new $pdoClass($modx, $scriptProperties);
+if ($pdoClass = $modx->loadClass($fqn, '', false, true)) {
+	$pdoFetch = new $pdoClass($modx, $scriptProperties);
+}
+elseif ($pdoClass = $modx->loadClass($fqn, MODX_CORE_PATH . 'components/pdotools/model/', false, true)) {
+	$pdoFetch = new $pdoClass($modx, $scriptProperties);
+}
+else {
+	$modx->log(modX::LOG_LEVEL_ERROR, 'Could not load pdoFetch from "MODX_CORE_PATH/components/pdotools/model/".');
+	return false;
+}
 $pdoFetch->addTime('pdoTools loaded');
 
 if (isset($parents) && $parents === '') {
--- core/components/tickets/elements/snippets/snippet.ticket_meta.php
+++ core/components/tickets/elements/snippets/snippet.ticket_meta.php
@@ -7,8 +7,16 @@ $Tickets->initialize($modx->context->key, $scriptProperties);
 $scriptProperties['nestedChunkPrefix'] = 'tickets_';
 /** @var pdoFetch $pdoFetch */
 $fqn = $modx->getOption('pdoFetch.class', null, 'pdotools.pdofetch', true);
-if (!$pdoClass = $modx->loadClass($fqn, '', false, true)) {return false;}
-$pdoFetch = new $pdoClass($modx, $scriptProperties);
+if ($pdoClass = $modx->loadClass($fqn, '', false, true)) {
+	$pdoFetch = new $pdoClass($modx, $scriptProperties);
+}
+elseif ($pdoClass = $modx->loadClass($fqn, MODX_CORE_PATH . 'components/pdotools/model/', false, true)) {
+	$pdoFetch = new $pdoClass($modx, $scriptProperties);
+}
+else {
+	$modx->log(modX::LOG_LEVEL_ERROR, 'Could not load pdoFetch from "MODX_CORE_PATH/components/pdotools/model/".');
+	return false;
+}
 $pdoFetch->addTime('pdoTools loaded');
 
 if (empty($id)) {$id = $modx->resource->id;}
