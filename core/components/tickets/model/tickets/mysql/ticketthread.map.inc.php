f9e4f3f76997e6279a7274ca683828927bd04c35
tolanych <tolanych@tut.by>
2019-09-10 17:30:08 +0300

    fix max key length with utf8mb4

--- core/components/tickets/model/tickets/mysql/ticketthread.map.inc.php
+++ core/components/tickets/model/tickets/mysql/ticketthread.map.inc.php
@@ -38,7 +38,7 @@ $xpdo_meta_map['TicketThread']= array (
     'name' => 
     array (
       'dbtype' => 'varchar',
-      'precision' => '255',
+      'precision' => '191',
       'phptype' => 'string',
       'null' => false,
       'default' => '',
