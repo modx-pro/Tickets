b4daa164f57ec89f061c2f0ae5f02c3ff84859ce
gvozdb <pavelgvozdb@yandex.ru>
2022-04-07 21:40:17 +0600

    1.13.1

--- _build/build.config.php
+++ _build/build.config.php
@@ -3,7 +3,7 @@
 // Define package
 define('PKG_NAME', 'Tickets');
 define('PKG_NAME_LOWER', strtolower(PKG_NAME));
-define('PKG_VERSION', '1.13.0');
+define('PKG_VERSION', '1.13.1');
 define('PKG_RELEASE', 'pl');
 define('PKG_AUTO_INSTALL', true);
 
