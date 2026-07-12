fc6640c1b1ed79515d7c8baf072adbc28d42d881
Ivan Bochkarev <ivanx86@gmail.com>
2026-07-12 12:46:54 +0600

    fix(lexicon): complete Ukrainian and clean English strings
    Fill missing uk keys for default/properties/settings/permissions, replace Russian leftovers in uk settings, fix the EN tickets_prop_resources/threads overwrite bug, and tighten broken English mgr/email copy.

--- core/components/tickets/lexicon/en/permissions.inc.php
+++ core/components/tickets/lexicon/en/permissions.inc.php
@@ -5,23 +5,23 @@
  * @package tickets
  * @subpackage lexicon
  */
-$_lang['ticket_save'] = 'Permission for save\update ticket.';
-$_lang['ticket_delete'] = 'Permission for delete own ticket.';
-$_lang['ticket_publish'] = 'Permission for publish or unpublish own ticket.';
-$_lang['ticket_vote'] = 'Permission for change rating of ticket.';
-$_lang['ticket_star'] = 'Permission for adding tickets to favorites.';
-$_lang['section_unsubscribe'] = 'Permission for unsubscribe users from sections tickets.';
-$_lang['ticket_view_private'] = 'Permission for reading private tickets.';
-$_lang['comment_save'] = 'Permission for save\update comments.';
-$_lang['comment_delete'] = 'Permission delete\undelete comments.';
-$_lang['comment_remove'] = 'Permission for remove comments with children without possibility to restore.';
-$_lang['comment_publish'] = 'Permission publish\unpublish comments.';
-$_lang['comment_file_upload'] = 'Permission for upload files in comment.';
-$_lang['comment_vote'] = 'Permission for change rating of comment.';
-$_lang['comment_star'] = 'Permission for adding comments to favorites.';
-$_lang['section_add_children'] = 'Permission for create new tickets in section.';
-$_lang['ticket_file_upload'] = 'Permission for upload files.';
-$_lang['ticket_file_delete'] = 'Permission for delete files.';
-$_lang['thread_close'] = 'Allows to close\open thread of comments.';
-$_lang['thread_delete'] = 'Allow delete\undelete the thread of comments.';
-$_lang['thread_remove'] = 'Allows permanently delete the thread with all the comments.';
+$_lang['ticket_save'] = 'Create and update tickets.';
+$_lang['ticket_delete'] = 'Delete own tickets.';
+$_lang['ticket_publish'] = 'Publish and unpublish own tickets.';
+$_lang['ticket_vote'] = 'Change ticket rating.';
+$_lang['ticket_star'] = 'Add tickets to favorites.';
+$_lang['section_unsubscribe'] = 'Unsubscribe users from ticket sections.';
+$_lang['ticket_view_private'] = 'View private tickets.';
+$_lang['comment_save'] = 'Create and update comments.';
+$_lang['comment_delete'] = 'Delete and restore comments.';
+$_lang['comment_remove'] = 'Permanently remove comments and their children.';
+$_lang['comment_publish'] = 'Publish and unpublish comments.';
+$_lang['comment_file_upload'] = 'Upload files in comments.';
+$_lang['comment_vote'] = 'Change comment rating.';
+$_lang['comment_star'] = 'Add comments to favorites.';
+$_lang['section_add_children'] = 'Create tickets in a section.';
+$_lang['ticket_file_upload'] = 'Upload ticket files.';
+$_lang['ticket_file_delete'] = 'Delete ticket files.';
+$_lang['thread_close'] = 'Close and open comment threads.';
+$_lang['thread_delete'] = 'Delete and restore comment threads.';
+$_lang['thread_remove'] = 'Permanently remove a thread and all its comments.';
--- core/components/tickets/lexicon/uk/permissions.inc.php
+++ core/components/tickets/lexicon/uk/permissions.inc.php
@@ -1,15 +1,27 @@
 <?php
 /**
- * Russian permissions Lexicon Entries for Tickets
+ * Ukrainian permissions Lexicon Entries for Tickets
  *
  * @package tickets
  * @subpackage lexicon
  */
-$_lang['ticket_save'] = 'Дозволяє створення\зміну тікету.';
-$_lang['ticket_delete'] = 'Дозволяє видалити свій тікет.';
-$_lang['ticket_publish'] = 'Дозволяє публікувати та знімати з публікації свій тікет.';
-$_lang['ticket_vote'] = 'Дозволяє змінювати рейтинг тікета.';
-$_lang['ticket_view_private'] = 'Дозволяє перегляд закритих тікетів.';
-$_lang['comment_save'] = 'Дозволяє створення\зміну коментаря.';
-$_lang['comment_vote'] = 'Дозволяє змінювати рейтинг коментаря.';
-$_lang['section_add_children'] = 'Дозволяє створювати тікети в секції.';
+$_lang['ticket_save'] = 'Створювати й змінювати тікети.';
+$_lang['ticket_delete'] = 'Видаляти власні тікети.';
+$_lang['ticket_publish'] = 'Публікувати й знімати з публікації власні тікети.';
+$_lang['ticket_vote'] = 'Змінювати рейтинг тікета.';
+$_lang['ticket_star'] = 'Додавати тікети в обране.';
+$_lang['section_unsubscribe'] = 'Відписувати користувачів від розділів тікетів.';
+$_lang['ticket_view_private'] = 'Переглядати приватні тікети.';
+$_lang['comment_save'] = 'Створювати й змінювати коментарі.';
+$_lang['comment_delete'] = 'Видаляти й відновлювати коментарі.';
+$_lang['comment_remove'] = 'Остаточно видаляти коментарі разом із вкладеними.';
+$_lang['comment_publish'] = 'Публікувати й знімати з публікації коментарі.';
+$_lang['comment_file_upload'] = 'Завантажувати файли в коментарях.';
+$_lang['comment_vote'] = 'Змінювати рейтинг коментаря.';
+$_lang['comment_star'] = 'Додавати коментарі в обране.';
+$_lang['section_add_children'] = 'Створювати тікети в розділі.';
+$_lang['ticket_file_upload'] = 'Завантажувати файли тікета.';
+$_lang['ticket_file_delete'] = 'Видаляти файли тікета.';
+$_lang['thread_close'] = 'Закривати й відкривати гілки коментарів.';
+$_lang['thread_delete'] = 'Видаляти й відновлювати гілки коментарів.';
+$_lang['thread_remove'] = 'Остаточно видаляти гілку з усіма коментарями.';
