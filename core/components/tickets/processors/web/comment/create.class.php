0a329f8cfd5b7fb519dea4c7d35ad76e5860a161
Ivan Bochkarev <ivanx86@gmail.com>
2026-07-12 11:34:45 +0600

    fix: restrict custom comment date to Administrator (#188)
    Validate Y-m-d H:i:s. Unauthorized date values are ignored.

--- core/components/tickets/processors/web/comment/create.class.php
+++ core/components/tickets/processors/web/comment/create.class.php
@@ -87,10 +87,24 @@ class TicketCommentCreateProcessor extends modObjectCreateProcessor
         unset($properties['requiredFields']);
 
         // Comment values
-        if ($this->getProperty('date')) {
-            $date = $this->getProperty('date');
-        } else {
-            $date = date('Y-m-d H:i:s');
+        $date = date('Y-m-d H:i:s');
+        $customDate = trim((string)$this->getProperty('date', ''));
+        $this->unsetProperty('date');
+        if ($customDate !== '') {
+            // Import-only: prevent public spoofing of createdon
+            $canSetDate = $this->modx->user->isMember('Administrator');
+            if ($canSetDate) {
+                $dt = \DateTime::createFromFormat('Y-m-d H:i:s', $customDate);
+                $errors = \DateTime::getLastErrors();
+                $valid = $dt instanceof \DateTime
+                    && $dt->format('Y-m-d H:i:s') === $customDate
+                    && empty($errors['warning_count'])
+                    && empty($errors['error_count']);
+                if (!$valid) {
+                    return $this->modx->lexicon('ticket_err_form');
+                }
+                $date = $customDate;
+            }
         }
         $ip = $this->modx->request->getClientIp();
         $this->setProperties(array(
